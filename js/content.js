e5b0f6296a0b13f603ca29fe93dec7b41da62db7
Kyosuke HAGIWARA <hagiwara-ky@rittor-music.co.jp>
2018-07-09 14:07:47 +0900

    almost all page discountrater

--- js/content.js
+++ js/content.js
@@ -1,31 +1,4 @@
-// let item_list = document.getElementsByClassName("itemStateIn");
-// // ccc = document.getElementsByClassName("p-items_wrapper");
-// // ccc.style.backgroundColor = "red";
-//
-// // alert(item_list.length);
-// console.log(item_list.length);
-//
-//
-// // item_list.forEach(function( value ) {
-// //     value.insertBefore(dp, value.firstChild);
-// // });
-// //
-// // for(let i=0; i<item_list.length; i++){
-// //     // document.body.style.backgroundColor = "red";
-// //     item_list[i].appendChild(dp);
-// //     // item_list[i].innerHTML = "<p>hhhheeeellllllllooo</p>" ;
-// //     console.log(item_list[i]);
-// // }
-// //
-// // let parents = $('.it-Header');
-// // console.log(parents.length);
-// //
-// // for(let i=0; i<parents.length; i++){
-// //     console.log("kita");
-// //     parents[i].after("<b>Hello</b>");
-// // }
 
-// console.log("hogo");
 let parent = $('.itemStateIn');
 for(let i=0; i<parent.length; i++){
     let fp = parent[i].getElementsByClassName("fixedPrice");
@@ -40,14 +13,11 @@ for(let i=0; i<parent.length; i++){
     if(String(fp[0].innerHTML).indexOf("割引率") === -1){
         fp[0].innerHTML = "割引率 " + String(p_down) + "%<br>" + fp[0].innerHTML;
     }else{
-        console.log("dadadada");
+        console.log("mouaru");
     }
-
 }
 
 let parents = $('.itemState');
-// console.log(parents);
-// console.log("aaasss");
 for(let i=0; i<parents.length; i++){
     console.log("kita");
     let fp = parents[i].getElementsByClassName("fixedPrice");
@@ -59,14 +29,10 @@ for(let i=0; i<parents.length; i++){
     console.log(l_price);
 
     let p_down = parseInt(100 * (1 - l_price / h_price));
-    // console.log(fp[0].innerHTML);
     if(String(fp[0].innerHTML).indexOf("割引率") === -1){
         fp[0].innerHTML = "割引率 " + String(p_down) + "%<br>" + fp[0].innerHTML;
     }else{
-        console.log("dadadada");
+        console.log("mouaru");
     }
-
-    // fp[0].innerHTML = "割引率 " + String(p_down) + "%<br>" + fp[0].innerHTML;
-
 }
 console.log(parent.length);
